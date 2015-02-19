c5f15ddf9bc3700358191315dd7169f7df7ee489
Chet Mancini <chetmancini@gmail.com>
2015-02-19 13:00:31 -0500

    CM: add project.clj

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject clojure-art "0.1.0-SNAPSHOT"
+  :description "REPL me some art"
+  :url "http://github.com/chetmancini/clojure-art"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]])
